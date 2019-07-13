13b43d0fe57d4b04b4919f3ac92336c81581bbea
Nimish Bongale <nimishnb98@gmail.com>
2019-07-13 12:52:17 +0530

    Further updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var neo4j = require('neo4j-driver').v1
+var neo4j = require('neo4j-driver').v1;
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'ibmhack'))
+var driver = neo4j.driver('bolt://hobby-nimcknmcieflgbkepkpnpidl.dbs.graphenedb.com:24787', neo4j.auth.basic('myapp', 'b.yntraNmvs9zf.OB5EVHINaBBWtc3M'))
 var session = driver.session()
 
 app.get('/', function (req, res) {
@@ -44,17 +44,38 @@ app.post('/person/add', function (req, res) {
     var name1 = req.body.name;
     var rol1 = req.body.role
     var org1 = req.body.org;
+    var type1 = req.body.type;
 
-    session
-        .run("CREATE(n:person {name:{nameParam},role:{rolParam},organization:{orgParam}}) RETURN n", { rolParam: rol1, nameParam: name1, orgParam: org1 })
-        .then(function (result) {
-            res.redirect('/')
-            session.close()
-        })
-        .catch(function (err) {
-            console.log(err)
-        })
-
+    if (type1 === "person")
+        session
+            .run("CREATE(n:person {name:{nameParam},role:{rolParam},organization:{orgParam}}) RETURN n", { rolParam: rol1, nameParam: name1, orgParam: org1 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else if (type1 === "pet")
+        session
+            .run("CREATE(n:pet {name:{nameParam},role:{rolParam},organization:{orgParam}}) RETURN n", { rolParam: rol1, nameParam: name1, orgParam: org1 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else
+        session
+            .run("CREATE(n:other {name:{nameParam},role:{rolParam},organization:{orgParam}}) RETURN n", { rolParam: rol1, nameParam: name1, orgParam: org1 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
     res.redirect('/')
 })
 
@@ -69,6 +90,7 @@ app.post('/person/del', function (req, res) {
         .catch(function (err) {
             console.log(err)
         })
+    res.redirect('/')
 })
 
 app.post('/person/link', function (req, res) {
@@ -76,15 +98,67 @@ app.post('/person/link', function (req, res) {
     var connect = req.body.connect;
     var name2 = req.body.name2;
 
-    session
-        .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:friend]->(b)", { name1Param: name1, name2Param: name2 })
-        .then(function (result) {
-            res.redirect('/')
-            session.close()
-        })
-        .catch(function (err) {
-            console.log(err)
-        })
+    if (connect === "friend")
+        session
+            .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:friend]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else if (connect === "relative")
+        session
+            .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:relative]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else if (connect === "neighbour")
+        session
+            .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:neighbour]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else if (connect === "colleague")
+        session
+            .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:colleague]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else if (connect === "owner")
+        session
+            .run("MATCH (a:person),(b:pet) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:owner]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    else
+        session
+            .run("MATCH (a:person),(b:person) WHERE a.name = {name1Param} AND b.name = {name2Param} CREATE (a)-[r:other]->(b)", { name1Param: name1, name2Param: name2 })
+            .then(function (result) {
+                res.redirect('/')
+                session.close()
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    res.redirect('/')
 })
 
 app.listen(3000)
